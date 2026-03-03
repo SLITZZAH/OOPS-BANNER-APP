96319501b4ad80cdd883b3387e20ebc76469c89f
Shaurya <singhshaurya27@gmail.com>
2026-03-03 09:03:02 +0530

    initial commit

--- OOPS_Banner.java
+++ OOPS_Banner.java
@@ -1,11 +1,11 @@
-public class OOPS_Banner {
-    public static void main(String[] args) {
-        System.out.println("   ***   " + "  " + "   ***    " + "  " + "******  " + "  " + "  *****    " );
-        System.out.println(" **   ** " + "  " + " **   **  " + "  " + "**    **" + "  " + " **    **  " );
-        System.out.println("**     **" + "  " + "**     ** " + "  " + "**    **" + "  " + " **        " );
-        System.out.println("**     **" + "  " + "**     ** " + "  " + "******  " + "  " + "  *****    " );
-        System.out.println("**     **" + "  " + "**     ** " + "  " + "**      " + "  " + "       **  " );
-        System.out.println(" **  **  " + "  " + " **   **  " + "  " + "**      " + "  " + " **    **  " );
-        System.out.println("   ***   " + "  " + "   ***    " + "  " + "**      " + "  " + "  *****    " );
+public class OOPS_banner {
+    public static void main(String[] args){
+        System.out.println(String.join(" ", "   **     ", "   **   ","  ***    ", "  ***  "));
+        System.out.println(String.join(" ", " *    *   ", " *     *  ", "*   * ", "*     *"));
+        System.out.println(String.join(" ", "*       * ", "*       * ", "*   * ", "**      "));
+        System.out.println(String.join(" ", "*       * ", "*       * ", "***   ", "  ***  "));
+        System.out.println(String.join(" ", "*       * ", "*       * ", "*     ", "      * "));
+        System.out.println(String.join(" ", " *     *  ", " *     *  ", "*     ", "*    **"));
+        System.out.println(String.join(" ", "   **     ", "   **     ", "*     ", "  ***   "));
     }
 }
